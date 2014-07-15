0ccd035cdb0c8371b9be5fb8b15e68f5aa0837a9
Chris Belanger <chris@chrisbelanger.net>
2014-07-15 07:09:25 -0400

    got urls working

--- dartagnan/settings.py
+++ dartagnan/settings.py
@@ -119,7 +119,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'dartagnan',
     'dartagnan.menu'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
