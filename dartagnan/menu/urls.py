d462549423a05dcd9ecad4e62b49db5a4c7c05de
Chris Belanger <chris@chrisbelanger.net>
2014-08-17 14:33:55 -0400

    Added menu navigation and about us and location html skeleton

--- dartagnan/menu/urls.py
+++ dartagnan/menu/urls.py
@@ -4,5 +4,7 @@ from dartagnan.menu import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^category/(?P<category_name>\w+)', views.category, name='category')
+    url(r'^category/(?P<category_name>\w+)', views.category, name='category'),
+    url(r'^about$', views.about_us, name='about_us'),
+    url(r'^locations$', views.locations, name='locations')
 )
--- dartagnan/menu/views.py
+++ dartagnan/menu/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from dartagnan.menu.models import Category
+from dartagnan.menu.models import Category, RestaurantInfo, Location
 from dartagnan.menu.helpers import get_context
 from django.template import RequestContext, loader
 
@@ -13,5 +13,19 @@ def category(request, category_name):
     template = loader.get_template('category.html')
     context = get_context(request, {
         'category': selected_category
+    })
+    return HttpResponse(template.render(context))
+
+
+def about_us(request):
+    template = loader.get_template('aboutus.html')
+    context = get_context(request, {
+    })
+    return HttpResponse(template.render(context))
+
+
+def locations(request):
+    template = loader.get_template('locations.html')
+    context = get_context(request, {
     })
     return HttpResponse(template.render(context))
